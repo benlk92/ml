e222e4a8171826090961ba2245f35d84665e0037
Hunter Clarke <hunter@hunterc.com>
2015-01-21 22:28:54 -0500

    finished decision trees

--- Supervised Learning/DecisionTree.py
+++ Supervised Learning/DecisionTree.py
@@ -1,13 +1,63 @@
-from sklearn.datasets import load_iris
-from sklearn import tree
-from sklearn.externals.six import StringIO
+import time
+import datetime
+import csv
+import numpy as np
 import pydot
 
-iris = load_iris()
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(iris.data, iris.target)
+from sklearn import tree
+from sklearn.externals.six import StringIO
 
-dot_data = StringIO()
-tree.export_graphviz(clf, out_file=dot_data)
-graph = pydot.graph_from_dot_data(dot_data.getvalue())
-graph.write_pdf("iris.pdf")
+from HeartDiseaseParser import HeartDiseaseParser
+
+class DecisionTree():
+    def __init__(self, parser):
+        self.parser = parser
+        self.classifier = tree.DecisionTreeClassifier()
+
+    def train(self, data, target):
+        self.classifier = self.classifier.fit(data, target)
+
+    def print_tree(self, filename):
+        dot_data = StringIO()
+        tree.export_graphviz(self.classifier, out_file=dot_data, feature_names=self.parser.ATTRIBUTES)
+        graph = pydot.graph_from_dot_data(dot_data.getvalue())
+        graph.write_pdf(filename)
+
+    def test(self, test_percentage):
+        data = self.parser.parse()
+        training_data_length = int(len(data['data']) * (1 - test_percentage))
+
+        training_data = data['data'][0:training_data_length,:]
+        training_target = data['target'][0:training_data_length]
+
+        self.train(training_data, training_target)
+
+        test_data = data['data'][training_data_length:,:]
+        test_target = data['target'][training_data_length:]
+
+        results = self.classifier.predict(test_data)
+
+        num_correct = 0
+        for i in range(len(results)):
+            if test_target[i] == results[i]:
+                num_correct = num_correct + 1
+
+        print "Number of tests:", len(results)
+        print "Number correct:", num_correct
+        print "--------------------------------------"
+        print "Percentage correct:", (num_correct / float(len(results)))*100
+        
+        current_time = time.time()
+        timestamp = datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d_%H:%M:%S')
+        self.print_tree("test_" + timestamp + ".pdf")
+
+if __name__ == '__main__':
+    heart_disease_parser = HeartDiseaseParser('cleve.mod')
+    heart_disease_decision_tree = DecisionTree(heart_disease_parser)
+    heart_disease_decision_tree.test(.1)
+
+
+
+
+
+
---
+++ Supervised Learning/HeartDiseaseParser.py
@@ -0,0 +1,102 @@
+import numpy as np
+import csv
+
+class HeartDiseaseParser():
+
+    ATTRIBUTES = [
+        'Age',
+        'Sex',
+        'Chest Pain Type',
+        'Resting Blood Pressure',
+        'Cholesteral',
+        'Fasting Blood Pressure < 120',
+        'Resting ECG',
+        'Max Heart Rate',
+        'Exercise Induced Angina',
+        'Oldpeak',
+        'Slope',
+        'Number of Vessels',
+        'Thal',
+        'Heart Disease?'
+    ]
+
+    ATTRIBUTE_FORMATS = [
+        np.uint,
+        np.bool_, # male ? 1 : 0
+        np.uint, # (angina, abnang, notang, asympt)
+        np.uint,
+        np.uint,
+        np.bool_,
+        np.uint, # (norm, abn, hyper)
+        np.uint,
+        np.bool_,
+        np.float_,
+        np.uint, # (up, flat, down)
+        np.float_,
+        np.uint, # (norm, fixed, rever)
+        np.bool_
+    ]
+
+    ATTRIBUTE_VALUE_NAMES = {
+        1: ['female', 'male'],
+        2: ['angina', 'abnang', 'notang', 'asympt'],
+        6: ['norm', 'abn', 'hyper'],
+        10: ['up', 'flat', 'down'],
+        12: ['norm', 'fixed', 'rever']
+    }
+
+    TARGET_NAMES = [
+        'Has heart disease',
+        'Does not have heart disease'
+    ]
+
+    def __init__(self, filename):
+        self.filename = filename 
+
+    def preprocess(self, row):
+        row[0] = int(float(row[0]))
+        row[1] = {'fem':0, 'male':1}[row[1]]
+        row[2] = {'angina':0, 'abnang':1, 'notang':2, 'asympt':3}[row[2]]
+        row[3] = int(float(row[3]))
+        row[4] = int(float(row[4]))
+        row[5] = {'fal':0, 'true':1}[row[5]]
+        row[6] = {'norm':0, 'abn':1, 'hyp':2}[row[6]]
+        row[7] = int(float(row[7]))
+        row[8] = {'fal':0, 'true':1}[row[8]]
+        row[9] = float(row[9])
+        row[10] = {'up':0, 'flat':1, 'down':2}[row[10]]
+        row[11] = int(float(row[11]))
+        row[12] = {'norm':0, 'fix':1, 'rev':2}[row[12]]
+        row[13] = 0 if row[13] == 'buff' else 1
+        row.pop()
+        return row
+
+    def parse(self):
+        data = []
+
+        with open(self.filename, 'rb') as datafile:
+            reader = csv.reader(datafile, delimiter=' ')
+
+            for idx,row in enumerate(reader):
+                # only start preprocessing after comments
+                if idx < 20 or len(row) == 0:
+                    continue
+
+                data.append(self.preprocess(row))
+
+        data = np.array(data)
+
+        return {
+            'data': data[:,0:-1],
+            'target': data[:,-1]
+        }
+
+
+
+
+
+
+
+
+
+
